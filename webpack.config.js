5ba7a9dcdee751f37801168475e81f669cbc27c5
Lob2018 <@>
2022-09-15 16:27:45 +0200

    Fix : Failed to load favicon.ico and manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { merge } = require('webpack-merge')
 const modeConfiguration = (env) => require(`./build-utils/webpack.${env}`)(env)
 
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
+const options = {
+  short_name: 'React App',
+  name: 'Create React App Sample',
+  icons: [
+    {
+      src: 'favicon.ico',
+      sizes: '64x64 32x32 24x24 16x16',
+      type: 'image/x-icon',
+    },
+    {
+      src: 'logo192.png',
+      type: 'image/png',
+      sizes: '192x192',
+    },
+    {
+      src: 'logo512.png',
+      type: 'image/png',
+      sizes: '512x512',
+    },
+  ],
+  start_url: '.',
+  display: 'standalone',
+  theme_color: '#000000',
+  background_color: '#ffffff',
+}
+
 module.exports = ({ mode } = { mode: 'production' }) => {
   console.log(`mode is: ${mode}`)
 
@@ -18,7 +45,7 @@ module.exports = ({ mode } = { mode: 'production' }) => {
           {
             test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg|ico)(\?[a-z0-9=.]+)?$/,
             exclude: /node_modules/,
-            use: ['url-loader', 'file-loader'],
+            use: ['url-loader', 'file-loader?name=[name].[ext]'],
           },
           {
             test: /\.(js|jsx)$/,
@@ -39,7 +66,10 @@ module.exports = ({ mode } = { mode: 'production' }) => {
       plugins: [
         new HtmlWebpackPlugin({
           template: './public/index.html',
+          filename: './index.html',
+          favicon: './public/favicon.ico',
         }),
+        new WebpackManifestPlugin(options),
       ],
       resolve: {
         extensions: ['.tsx', '.ts', '.js'],
