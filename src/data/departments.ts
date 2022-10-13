a7b7729f01e3bace33a1655fa18ce3fb9d69a686
Lob2018 <@>
2022-10-13 16:35:16 +0200

    Refactor : Moves dropdown's data to src/data

---
+++ src/data/departments.ts
@@ -0,0 +1,10 @@
+import Department from '../interfaces/data.department.interface'
+
+const departments: Department[] = [
+  { value: 'Sales' },
+  { value: 'Marketing' },
+  { value: 'Engineering' },
+  { value: 'Human Resources' },
+  { value: 'Legal' },
+]
+export default departments
